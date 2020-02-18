ec406de384ef367ce01ea35fcc60722031a70a47
missTinuade <estherjolugba@yahoo.com>
2020-02-18 11:45:32 +0000

    Initial commit

---
+++ app/src/main/java/com/example/rickandmortchar/MainActivity.java
@@ -0,0 +1,19 @@
+/***
+ * Copyright (c) 2020 Esther Jolugba
+ * All rights reserved
+ * Created on Feb 16-02-2020
+ */
+package com.example.rickandmortchar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
